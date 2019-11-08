720c0c01e85871748a20823952136aa41a3269ab
Amit <d00417346@dmail.dixie.edu>
2019-11-08 15:28:34 -0500

    fixed indent

--- reader.py
+++ reader.py
@@ -8,7 +8,7 @@ def read_folder(folder_name):
         for file in files:
             file_list.append(os.path.join(subdir, file))
 
-            song_info = []
+    song_info = []
 
     for file in file_list:
         with open(file, 'r') as json_file:
